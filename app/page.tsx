890b8695254bc1034699e135599d442b6dcab563
Franco Avolio <francoavolio@hotmail.com>
2024-11-18 20:08:17 -0300

    add carousel to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { TrendingItems } from "@/components/ui/Home/TrendingItems";
 import { Banner } from "@/components/ui/Home/ui/Banner";
+import { TrendingProducts } from "@/components/ui/Home/ui/TrendingProducts";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <TrendingProducts />
       <TrendingItems />
     </div>
   );
