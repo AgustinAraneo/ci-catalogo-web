788d910b738348805f6fa92736f2e754f7d8b729
Alan Lampert <60844186+alan25593@users.noreply.github.com>
2024-11-23 12:04:58 -0300

    change upload button

--- app/admin/ui/ProductForm.tsx
+++ app/admin/ui/ProductForm.tsx
@@ -195,12 +195,27 @@ export const ProductForm: React.FC<ProductFormProps> = ({ onAddProduct }) => {
           {/* Botón de carga de imagen */}
           <div className="md:col-span-2">
             <Label htmlFor="image">Imagen</Label>
-            <Input
-              type="file"
-              id="image"
-              name="image"
-              onChange={handleFileChange}
-            />
+            <div className="flex items-center space-x-4 mt-2">
+              {/* Input de archivo oculto */}
+              <input
+                type="file"
+                id="image"
+                name="image"
+                onChange={handleFileChange}
+                className="hidden"
+              />
+              {/* Botón para abrir el selector de archivo */}
+              <label
+                htmlFor="image"
+                className="px-4 py-2 bg-blue-500 text-white rounded-lg cursor-pointer hover:bg-blue-600"
+              >
+                Subir Imagen
+              </label>
+              {/* Texto indicando el archivo seleccionado */}
+              <span className="text-gray-600 text-sm">
+                {file ? file.name : "Ningún archivo seleccionado"}
+              </span>
+            </div>
             {uploadProgress > 0 && (
               <div className="mt-2 w-full bg-gray-200 rounded-full h-2.5">
                 <div
@@ -210,6 +225,7 @@ export const ProductForm: React.FC<ProductFormProps> = ({ onAddProduct }) => {
               </div>
             )}
           </div>
+
           {/* Talles */}
           <div className="md:col-span-2">
             <Label>Talles</Label>
