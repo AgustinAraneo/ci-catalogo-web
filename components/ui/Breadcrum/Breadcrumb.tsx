605ace25a1d08b90bab6d7791d14b10a3519455b
Alan Lampert <60844186+alan25593@users.noreply.github.com>
2024-11-25 20:04:04 -0300

    breadcrumb mobile

--- components/ui/Breadcrum/Breadcrumb.tsx
+++ components/ui/Breadcrum/Breadcrumb.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export const Breadcrumb = ({ page }: { page: string }) => {
   return (
-    <div className="w-full h-[450px] bg-gray-200 items-center justify-center flex flex-col gap-4 shadow-redcoach-lg ">
-      <h2 className="text-6xl font-bold">{page}</h2>
-      <div>
-        <p className="font-semibold">
+    <div className="w-full h-[450px] bg-gray-200 items-center justify-center flex flex-col gap-4 shadow-redcoach-lg">
+      <h2 className="text-6xl sm:text-3xl text-center font-bold">{page}</h2>
+      <div className="text-center">
+        <p className="font-semibold text-base sm:text-sm">
           <Link href={"/"} className="hover:text-gold transition-all">
             Inicio
           </Link>{" "}
